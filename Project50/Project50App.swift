8b8d2a9d6e61bf605b10ceaedbfc42451da831e4
Jean-Baptiste DELCROS <jbdelcros4@hotmail.fr>
2022-12-11 09:55:15 -0500

    Initial Commit

---
+++ Project50/Project50App.swift
@@ -0,0 +1,17 @@
+//
+//  Project50App.swift
+//  Project50
+//
+//  Created by jean-baptiste delcros on 11/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
